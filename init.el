14312c95797a439b1c9364b782dba5eef436ce78
Abhishek Amralkar <abhishekamralkar@gmail.com>
2022-10-07 22:28:36 +0530

    added python jedi, yasnippet, jedi

--- init.el
+++ init.el
@@ -10,4 +10,17 @@
   (package-install 'use-package))
 
 
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org")
+(org-babel-load-file "~/.emacs.d/myinit.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(elpy jedi cmake-mode zenburn-theme which-key use-package try orgalist org-bullets flycheck counsel color-theme auto-complete ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
