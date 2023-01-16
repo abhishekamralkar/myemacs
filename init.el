81a2d592f63c3e45ff1b7d2bef863f67fbf91c34
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-01-16 23:55:53 +0530

    update theme and modeline

--- init.el
+++ init.el
@@ -24,22 +24,7 @@
                           :append :local)))
 
 (use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-palenight t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+  :init (load-theme 'doom-palenight t))
 
 (font-family-list)
 (add-to-list 'default-frame-alist
@@ -113,7 +98,8 @@
 
 (use-package doom-modeline
   :ensure t
-  :init (doom-modeline-mode 1))
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
 (use-package all-the-icons
   :ensure t)
