2823c7c81c0b3b775de0f44f9719337b9ae7a98f
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-01-17 16:45:23 +0530

    add function to create a python template when new *.py created. just trying to be more lazy

--- init.el
+++ init.el
@@ -263,10 +263,24 @@
    :config
      (pyvenv-mode 1))
 
-(use-package company-jedi
-  :ensure t
-  :config
-  (add-to-list 'company-backends 'company-jedi))
+;;(use-package company-jedi
+;;  :ensure t
+;;  :config
+;;  (add-to-list 'company-backends 'company-jedi))
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
 
 (defun pythontemplate()
    "Insert template for python"
