f4366faa884e37877fdd32e8bd9a0639d72f40f1
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-01-19 23:47:50 +0530

    emacs: update developer tools and UI configuration

--- init.el
+++ init.el
@@ -105,3 +105,136 @@
     (if window-system
       (fancy-battery-mode)
       (display-battery-mode)))
+
+(use-package which-key
+  :ensure t
+  :config
+    (which-key-mode))
+
+(use-package command-log-mode
+  :commands command-log-mode)
+
+(use-package async
+  :ensure t
+  :init (dired-async-mode 1))
+
+(use-package ivy
+  :ensure t)
+(setq scroll-conservatively 100)
+
+(use-package swiper
+  :ensure t
+  :bind ("C-s" . 'swiper))
+
+(use-package helm
+:straight t
+:bind
+  ("C-x C-f" . 'helm-find-files)
+  ("C-x C-b" . 'helm-buffers-list)
+  ("M-x" . 'helm-M-x)
+:config)
+
+(setq electric-pair-pairs '(
+                           (?\{ . ?\})
+                           (?\( . ?\))
+                           (?\[ . ?\])
+                           (?\" . ?\")
+                           ))   
+(electric-pair-mode t)
+
+(show-paren-mode 1)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+    (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+
+;; (use-package company
+  ;;   :ensure
+  ;;   :custom
+  ;;   (company-idle-delay 0.5) ;; how long to wait until popup
+  ;;   ;; (company-begin-commands nil) ;; uncomment to disable popup
+  ;;   :bind
+  ;;   (:map company-active-map
+  ;; 		("C-n". company-select-next)
+  ;; 		("C-p". company-select-previous)
+  ;; 		("M-<". company-select-first)
+  ;; 		("M->". company-select-last)))
+
+(use-package company
+   :after lsp-mode
+   :hook (lsp-mode . company-mode)
+   :bind (:map company-active-map
+	  ("<tab>" . company-complete-selection))
+	  (:map lsp-mode-map
+	  ("<tab>" . company-indent-or-complete-common))
+   :custom
+   (company-minimum-prefix-length 1)
+   (company-idle-delay 0.0))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package yasnippet
+  :ensure t
+  :config
+    (use-package yasnippet-snippets
+      :ensure t)
+    (yas-reload-all))
+
+(use-package magit
+  :ensure t
+  :bind ("C-x g" . magit))
+  
+(use-package forge
+  :ensure t
+  :after magit)
+
+(use-package projectile
+  :ensure t
+  :init
+    (projectile-mode 1))
+
+(use-package beacon
+  :ensure t
+  :config
+    (beacon-mode 1))
+
+(defun lsp-mode-setup ()
+  (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
+  (lsp-headerline-breadcrumb-mode))
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :hook (lsp-mode . lsp-mode-setup)
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom)
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy
+  :after lsp)
