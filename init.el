57b7763085b0b31cc4113b788c25e4dc609a5256
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-09-28 23:25:07 -0700

    [emacs] update emacs UI enhancement

--- init.el
+++ init.el
@@ -1,11 +1,10 @@
 (setq user-full-name "Abhishek Anand Amralkar"
-      user-mail-address "abhishekamralkar@gmail.com")
+  user-mail-address "abhishekamralkar@gmail.com")
 
 (require 'package)
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			   ("org"   . "https://orgmode.org/elpa/")
-			   ("elpa"  . "https://elpa.gnu.org/packages/")))
+			  ("org"   . "https://orgmode.org/elpa/")
+			  ("elpa"  . "https://elpa.gnu.org/packages/")))
 
 (package-initialize)
 (unless package-archive-contents
@@ -15,9 +14,9 @@
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-(require 'org-tempo)
+	(require 'use-package)
+	(setq use-package-always-ensure t)
+	(require 'org-tempo)
 
 (use-package auto-package-update
   :custom
@@ -29,17 +28,69 @@
   (auto-package-update-at-time "09:00"))
 
 (use-package doom-themes
-    :defer t
-    :init (load-theme 'doom-palenight t))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(doom-themes use-package auto-package-update)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  :defer t
+  :init (load-theme 'doom-palenight t))
+
+(set-face-attribute 'default nil :font "JetBrains Mono NL" :height 140 :weight 'regular)
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono NL" :height 140 :weight 'regular)
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "JetBrains Mono NL" :height 140 :weight 'regular)
+
+(use-package ac-emoji
+  :ensure t)
+
+(setq inhibit-startup-message t)
+
+(tool-bar-mode -1)
+
+(menu-bar-mode -1)
+
+(scroll-bar-mode -1)
+
+(setq ring-bell-function 'ignore)
+
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(use-package dashboard
+  :ensure t
+  :config
+    (dashboard-setup-startup-hook))
+
+;; Set the title
+(setq dashboard-banner-logo-title "Welcome to AAA Emacs ")
+
+;; Content is not centered by default. To center, set
+(setq dashboard-center-content t)
+
+;; To disable shortcut "jump" indicators for each section, set
+(setq dashboard-show-shortcuts nil)
+
+(setq dashboard-items '((recents  . 5)
+                        (bookmarks . 5)
+                        (projects . 5)
+                        (agenda . 5)
+                        (registers . 5)))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package all-the-icons
+  :ensure t)
+
+(use-package nerd-icons
+    :ensure t)
+
+(setq line-number-mode t)
+(setq column-number-mode t)
