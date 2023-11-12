c36741445cffe5be239a066f85cb472170ffdd5c
Abhishek Amralkar <abhishekamralkar@gmail.com>
2023-11-11 16:31:50 -0800

    [emacs] fix the name for the fints defvar

--- init.el
+++ init.el
@@ -151,6 +151,24 @@
   :config
   (which-key-mode))
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
