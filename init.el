06e40cafc608c6ebd075000bddfabc6fbc5f52c2
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-12-04 21:49:52 -0800

    [emacs] update emacs theme to modus theme from the dooms

--- init.el
+++ init.el
@@ -120,9 +120,28 @@
   :config
   (beacon-mode 1))
 
-(use-package doom-themes
-  :defer t
-  :init (load-theme 'doom-monokai-classic t))
+(use-package modus-themes
+    :ensure t
+    :config
+    (setq modus-themes-mode-line '(accented borderless)
+      modus-themes-bold-constructs t
+      modus-themes-italic-constructs t
+      modus-themes-fringes 'subtle
+      modus-themes-tabs-accented t
+      modus-themes-paren-match '(bold intense)
+      modus-themes-prompts '(bold intense)
+      modus-themes-completions 'opinionated
+      modus-themes-org-blocks 'tinted-background
+      modus-themes-scale-headings t
+      modus-themes-region '(bg-only)
+      modus-themes-headings
+      '((1 . (rainbow overline background 1.4))
+        (2 . (rainbow background 1.3))
+        (3 . (rainbow bold 1.2))
+        (t . (semilight 1.1))))
+
+;; Load the dark theme by default
+(load-theme 'modus-vivendi t))
 
 (set-frame-font "JetBrains Mono-16" nil t)
 
