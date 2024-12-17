a0e3ce0d630c1b90873443d9e1fd2720897c7abf
Abhishek Amralkar <abhishekamralkar@gmail.com>
2024-12-16 20:46:40 -0800

    update the ef-theme

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (setq gc-cons-threshold (* 50 1000 1000))
 
 (defun myemacs/display-startup-time ()
@@ -52,6 +51,8 @@
 (setq column-number-mode t)
 (global-display-line-numbers-mode)
 (global-hl-line-mode 1)
+;;(setq package-check-signature nil)
+;;(setq package-check-signature 'allow-unsigned)
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -121,30 +122,26 @@
   :config
   (beacon-mode 1))
 
-(use-package modus-themes
+(use-package ef-themes
     :ensure t
-    :config
-    (setq modus-themes-mode-line '(accented borderless)
-      modus-themes-bold-constructs t
-      modus-themes-italic-constructs t
-      modus-themes-fringes 'subtle
-      modus-themes-tabs-accented t
-      modus-themes-paren-match '(bold intense)
-      modus-themes-prompts '(bold intense)
-      modus-themes-completions 'opinionated
-      modus-themes-org-blocks 'tinted-background
-      modus-themes-scale-headings t
-      modus-themes-region '(bg-only)
-      modus-themes-headings
-      '((1 . (rainbow overline background 1.4))
-        (2 . (rainbow background 1.3))
-        (3 . (rainbow bold 1.2))
-        (t . (semilight 1.1))))
-
+    :config)
+(setq ef-themes-headings ; read the manual's entry or the doc string
+      '((0 variable-pitch light 1.9)
+        (1 variable-pitch light 1.8)
+        (2 variable-pitch regular 1.7)
+        (3 variable-pitch regular 1.6)
+        (4 variable-pitch regular 1.5)
+        (5 variable-pitch 1.4) ; absence of weight means `bold'
+        (6 variable-pitch 1.3)
+        (7 variable-pitch 1.2)
+        (t variable-pitch 1.1)))
+(setq ef-themes-mixed-fonts t
+      ef-themes-variable-pitch-ui t)
+(mapc #'disable-theme custom-enabled-themes)
 ;; Load the dark theme by default
-(load-theme 'modus-vivendi t))
+(load-theme 'ef-day :no-confirm)
 
-(set-frame-font "JetBrains Mono-16" nil t)
+(set-frame-font "JetBrains Mono-10" nil t)
 
 (use-package ac-emoji
   :ensure t)
@@ -522,515 +519,20 @@
                              (lambda ()
                                  (setq tab-width 2))))
 
-||||||| parent of d563bcc (use ef-themes)
-(setq gc-cons-threshold (* 50 1000 1000))
-
-(defun myemacs/display-startup-time ()
-  (message "Emacs loaded in %s with %d garbage collections."
-           (format "%.2f seconds"
-                   (float-time
-                     (time-subtract after-init-time before-init-time)))
-           gcs-done))
-
-(add-hook 'emacs-startup-hook #'myemacs/display-startup-time)
-
-(setq user-full-name "Abhishek Anand Amralkar"
-  user-mail-address "abhishekamralkar@gmail.com")
-
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			  ("org"   . "https://orgmode.org/elpa/")
-			  ("elpa"  . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents 
-  (package-refresh-contents))
-
-;; Initialize use-package on non-Linux platforms
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-
-(require 'use-package)
-(setq use-package-always-ensure t)
-(require 'org-tempo)
-(add-to-list 'org-structure-template-alist '("sh" . "src shell"))
-(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
-(add-to-list 'org-structure-template-alist '("py" . "src python"))
-
-(use-package auto-package-update
-  :custom
-  (auto-package-update-interval 7)
-  (auto-package-update-prompt-before-update t)
-  (auto-package-update-hide-results t)
-  :config
-  (auto-package-update-maybe)
-  (auto-package-update-at-time "09:00"))
-
-(setq inhibit-startup-message t)   ;no startup screen
-(tool-bar-mode -1)                 ;no toolbar
-(menu-bar-mode -1)                 ;no menubar
-(scroll-bar-mode -1)               ;no scrollbar
-(setq ring-bell-function 'ignore)  ;no ringing bells
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq line-number-mode t)
-(setq column-number-mode t)
-(global-display-line-numbers-mode)
-(global-hl-line-mode 1)
-
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
-(use-package dashboard
-  :ensure t
-  :config
-    (dashboard-setup-startup-hook))
-
-;; Set the title
-(setq dashboard-banner-logo-title "Welcome to AAA Emacs ")
-
-;; Content is not centered by default. To center, set
-(setq dashboard-center-content t)
-
-;; To disable shortcut "jump" indicators for each section, set
-(setq dashboard-show-shortcuts nil)
-
-(setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
-                        (registers . 5)))
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
-(use-package all-the-icons
-  :ensure t)
-
-(use-package nerd-icons
-    :ensure t)
-
-(use-package which-key
-  :ensure t
-  :config
-  (which-key-mode))
-
-(use-package dired-sidebar
-  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
-  :ensure t
-  :commands (dired-sidebar-toggle-sidebar)
-  :init
-  (add-hook 'dired-sidebar-mode-hook
-            (lambda ()
-              (unless (file-remote-p default-directory)
-                (auto-revert-mode))))
-  :config
-  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
-  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
-
-  (setq dired-sidebar-subtree-line-prefix "__")
-  (setq dired-sidebar-theme 'vscode)
-  (setq dired-sidebar-use-term-integration t)
-  (setq dired-sidebar-use-custom-font t))
-
-(use-package beacon
-  :ensure t
-  :config
-  (beacon-mode 1))
-
-(use-package doom-themes
-  :defer t
-  :init (load-theme 'doom-monokai-classic t))
-
-(set-frame-font "JetBrains Mono-16" nil t)
-
-(use-package ac-emoji
-  :ensure t)
-
-(use-package ligature
-  :config
-  ;; Enable the "www" ligature in every possible major mode
-  (ligature-set-ligatures 't '("www"))
-  ;; Enable traditional ligature support in eww-mode, if the
-  ;; `variable-pitch' face supports it
-  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-  ;; Enable all Cascadia Code ligatures in programming modes
-  (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
-                                       ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
-                                       "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
-                                       "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
-                                       "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
-                                       "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
-                                       "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
-                                       "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
-                                       ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
-                                       "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
-                                       "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
-                                       "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
-                                       "\\\\" "://"))
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(use-package ivy-rich
-  :after ivy
-  :init
-  (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind (("C-M-j" . 'counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history))
-  :custom
-  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
-  :config
-  (counsel-mode 1))
-
-(setq warning-minimum-level :emergency)
-
-(setq auto-save-default t)
-;; Set the interval between auto-saves based on time (in seconds)
-(setq auto-save-timeout 10)  ;; Save every 20 seconds of idle time
-
-(global-auto-revert-mode 1)
-;; Also auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-
-(add-hook 'org-mode-hook
-          (lambda ()
-            (add-hook 'before-save-hook 'org-babel-tangle nil t)))
-
-(show-paren-mode 1)
-
-(use-package rainbow-delimiters
-   :ensure t
-   :init
-   (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
-
-(use-package flycheck
-  :init (global-flycheck-mode))
-
-(use-package yasnippet
-   :ensure t
-   :config
-     (use-package yasnippet-snippets
-       :ensure t)
-     (yas-reload-all))
-
-(use-package magit
-     :ensure t
-     :bind ("C-x g" . magit))
-
-(use-package sqlite3
-  :ensure t)
-
-  (use-package forge
-     :ensure t
-     :after magit)
-
-(use-package projectile
-   :ensure t
-   :init
-     (projectile-mode 1))
-
-(use-package general
-   :ensure t)
-
-(use-package fzf
-  :bind
-    ;; Don't forget to set keybinds!
-  :config
-  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
-        fzf/executable "fzf"
-        fzf/git-grep-args "-i --line-number %s"
-        ;; command used for `fzf-grep-*` functions
-        ;; example usage for ripgrep:
-        ;; fzf/grep-command "rg --no-heading -nH"
-        fzf/grep-command "grep -nrH"
-        ;; If nil, the fzf buffer will appear at the top of the window
-        fzf/position-bottom t
-        fzf/window-height 15))
-
-(use-package exec-path-from-shell
-  :ensure t
-  :config
-  (when (memq window-system '(mac ns x))
-    (exec-path-from-shell-initialize)))
-
-(use-package electric
-  :ensure t
-  :config
-  (electric-pair-mode 1))
-
-(use-package company
-  :ensure t
-  :init
-  (add-hook 'after-init-hook 'global-company-mode)
-  :config
-  (setq company-idle-delay 0.2
-        company-minimum-prefix-length 1
-        company-show-numbers t
-        company-tooltip-align-annotations t
-        company-tooltip-flip-when-above t))
-
-(use-package projectile
-  :ensure t
-  :init
-  (projectile-mode 1))
-
-(use-package hydra
-  :defer t)
-
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
-
-(use-package helm
-  :ensure t
-  :bind
-  ("C-x C-f" . 'helm-find-files)
-  ("C-x C-b" . 'helm-buffers-list)
-  ("M-x" . 'helm-M-x)
-  :config
-  (defun daedreth/helm-hide-minibuffer ()
-    (when (with-helm-buffer helm-echo-input-in-header-line)
-      (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-        (overlay-put ov 'window (selected-window))
-        (overlay-put ov 'face
-                     (let ((bg-color (face-background 'default nil)))
-                       `(:background ,bg-color :foreground ,bg-color)))
-        (setq-local cursor-type nil))))
-  (add-hook 'helm-minibuffer-set-up-hook 'daedreth/helm-hide-minibuffer)
-  (setq helm-autoresize-max-height 0
-        helm-autoresize-min-height 40
-        helm-M-x-fuzzy-match t
-        helm-buffers-fuzzy-matching t
-        helm-recentf-fuzzy-match t
-        helm-semantic-fuzzy-match t
-        helm-imenu-fuzzy-match t
-        helm-split-window-in-side-p nil
-        helm-move-to-line-cycle-in-source nil
-        helm-ff-search-library-in-sexp t
-        helm-scroll-amount 8 
-        helm-echo-input-in-header-line t)
-  :init
-  (helm-mode 1))
-
-(helm-autoresize-mode 1)
-(define-key helm-find-files-map (kbd "C-b") 'helm-find-files-up-one-level)
-(define-key helm-find-files-map (kbd "C-f") 'helm-execute-persistent-action)
-
-(use-package lsp-mode
-:ensure t
-:hook ((go-mode . lsp)
-       (rust-mode . lsp)
-       (python-mode . lsp))
-:commands lsp)
-
-(use-package dap-mode
-  :ensure t)
-
-(use-package eglot
-  :ensure t
-  :config
-  ;; Associate eglot with Python and Go
-  (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
-  (add-to-list 'eglot-server-programs '(go-mode . ("gopls")))
-  (add-to-list 'eglot-server-programs '(rust-mode . ("rust-analyzer"))))
-
-;; Tree-sitter for enhanced syntax highlighting
-(use-package tree-sitter
-  :hook ((python-mode . tree-sitter-mode)
-         (python-mode . tree-sitter-hl-mode)
-         (go-mode . tree-sitter-mode)
-         (go-mode . tree-sitter-hl-mode)
-         (rust-mode . tree-sitter-mode)
-         (rust-mode . tree-sitter-hl-mode)))
-
-  (use-package tree-sitter-langs
-    :ensure t
-    :after tree-sitter)
-
-(use-package lsp-pyright
-  :hook
-  (python-mode . (lambda ()
-                   (require 'lsp-pyright)
-                   (lsp-deferred))))
-
-(use-package pyvenv
-  :ensure t
-  :init
-  (setenv "WORKON_HOME" "~/.venvs/")
-  :config
-  ;; (pyvenv-mode t)
-
-  ;; Set correct Python interpreter
-  (setq pyvenv-post-activate-hooks
-        (list (lambda ()
-                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python")))))
-  (setq pyvenv-post-deactivate-hooks
-        (list (lambda ()
-                (setq python-shell-interpreter "python3")))))
-
-(use-package blacken
-  :init
-  (setq-default blacken-fast-unsafe t)
-  (setq-default blacken-line-length 80))
-
-(use-package python-mode
-  :hook
-  (python-mode . pyvenv-mode)
-  (python-mode . flycheck-mode)
-  (python-mode . company-mode)
-  (python-mode . blacken-mode)
-  (python-mode . yas-minor-mode)
-  :custom
-  ;; NOTE: Set these if Python 3 is called "python3" on your system!
-  (python-shell-interpreter "python3")
-  :config)
-
-(use-package go-mode
-    :ensure t)
-
-(setq exec-path (append exec-path '("/usr/local/go/bin/go")))
-
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
-
-(use-package clojure-mode
-   :defer t
-   :ensure t)
-
-(use-package cider
-  :ensure t)
-
-(use-package clj-refactor
-  :ensure t
-  :config
-  (add-hook 'clojure-mode-hook (lambda ()
-                                (clj-refactor-mode 1)
-                                ))
-  (cljr-add-keybindings-with-prefix "C-c C-m")
-  (setq cljr-warn-on-eval nil)
-   :bind ("C-c '" . hydra-cljr-help-menu/body))
-
-(add-hook 'shell-mode-hook 'yas-minor-mode)
-(add-hook 'shell-mode-hook 'flycheck-mode)
-(add-hook 'shell-mode-hook 'company-mode)
-
-(defun shell-mode-company-init ()
-  (setq-local company-backends '((company-shell
-                                  company-shell-env
-                                  company-etags
-                                  company-dabbrev-code))))
-
-(use-package company-shell
-  :ensure t
-  :config
-    (require 'company)
-    (add-hook 'shell-mode-hook 'shell-mode-company-init))
-
-(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
-(add-hook 'emacs-lisp-mode-hook 'yas-minor-mode)
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
-
-(use-package slime
-  :ensure t
-  :config
-  (setq inferior-lisp-program "/usr/bin/sbcl")
-  (setq slime-contribs '(slime-fancy)))
-
-(use-package slime-company
-  :ensure t
-  :init
-    (require 'company)
-    (slime-setup '(slime-fancy slime-company)))
-
-(use-package rust-mode
-  :ensure t
-  :hook
-  (rust-mode . eglot-ensure)
-  :config
-  (setq rust-format-on-save t))
-
-(use-package cargo
-  :ensure t
-  :hook (rust-mode . cargo-minor-mode))
-
-(use-package org-bullets
-    :hook (org-mode . org-bullets-mode)
-    :custom
-    (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(setq org-ellipsis " ")
-(setq org-src-fontify-natively t)
-(setq org-src-tab-acts-natively t)
-(setq org-confirm-babel-evaluate nil)
-(setq org-export-with-smart-quotes t)
-(setq org-src-window-setup 'current-window)
-(add-hook 'org-mode-hook 'org-indent-mode)
-
-(add-hook 'org-mode-hook
-            (lambda ()
-               (visual-line-mode 1)))
-
-(use-package diminish
-    :ensure t
-    :init
-    (diminish 'which-key-mode)
-    (diminish 'linum-relative-mode)
-    (diminish 'hungry-delete-mode)
-    (diminish 'visual-line-mode)
-    (diminish 'subword-mode)
-    (diminish 'beacon-mode)
-    (diminish 'irony-mode)
-    (diminish 'page-break-lines-mode)
-    (diminish 'auto-revert-mode)
-    (diminish 'rainbow-delimiters-mode)
-    (diminish 'rainbow-mode)
-    (diminish 'yas-minor-mode)
-    (diminish 'flycheck-mode)
-    (diminish 'helm-mode))
-
-(use-package json-mode
-   :ensure t
-   :config
-   (customize-set-variable 'json-mode-hook
-                             (lambda ()
-                                 (setq tab-width 2))))
-
-=======
->>>>>>> d563bcc (use ef-themes)
 (use-package yaml-mode
      :ensure t)
+
+(use-package docker
+     :ensure t
+     :bind (("C-c d c" . docker-containers)
+            ("C-c d i" . docker-images)))
+
+(use-package dockerfile-mode
+    :ensure t)
+
+(use-package kubernetes
+  :ensure t
+  :commands (kubernetes-overview))
 
 (use-package k8s-mode
   :ensure t
@@ -1038,3 +540,26 @@
 
 (use-package terraform-mode
     :ensure t)
+
+(use-package dired
+  :ensure nil
+  :commands (dired dired-jump)
+  :bind (("C-x C-j" . dired-jump))
+  :custom ((dired-listing-switches "-agho --group-directories-first")))
+
+(use-package dired-single
+  :commands (dired dired-jump))
+
+(use-package all-the-icons-dired
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package dired-open
+  :commands (dired dired-jump)
+  :config
+  ;; Doesn't work as expected!
+  ;;(add-to-list 'dired-open-functions #'dired-open-xdg t)
+  (setq dired-open-extensions '(("png" . "feh")
+                                ("mkv" . "mpv"))))
+
+(use-package dired-hide-dotfiles
+  :hook (dired-mode . dired-hide-dotfiles-mode))
